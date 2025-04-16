a2eadb104307f74854867a5664b52c8aadaaa78f
Ana Paula <apnl.lourenco@gmail.com>
2025-04-16 10:17:13 -0300

    commit

--- SIGAC/app/Models/nível.php
+++ SIGAC/app/Models/nível.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class aluno extends Model
+class nível extends Model
 {
     //
 }
---
+++ SIGAC/database/seeders/NívelSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class NívelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
